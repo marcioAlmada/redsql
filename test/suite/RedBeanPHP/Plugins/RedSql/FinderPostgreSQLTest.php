838103af1c58cf9a2f645c168a607b3bddf5a0e3
Márcio Almada <marcio3w@gmail.com>
2013-11-16 15:30:39 -0300

    fixing travis ci postgre coonnection credentials

--- test/suite/RedBeanPHP/Plugins/RedSql/FinderPostgreSQLTest.php
+++ test/suite/RedBeanPHP/Plugins/RedSql/FinderPostgreSQLTest.php
@@ -9,7 +9,7 @@ class FinderPostgreSQLTest extends FinderTest
 
     public function setUp()
     {
-        R::setup('pgsql:host=localhost;dbname=redsql','root');
+        R::setup('pgsql:host=localhost;dbname=redsql','postgres');
         parent::setup();
     }
 }
